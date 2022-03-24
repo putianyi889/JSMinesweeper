06af373962fefe05335f15257a064a4ef15e89fb
Tianyi Pu <912396513@qq.com>
2022-03-24 12:35:12 +0000

    remove dropHandler

--- Minesweeper/client/main.js
+++ Minesweeper/client/main.js
@@ -1602,43 +1602,7 @@ function on_mouseWheel_minesLeft(event) {
 }
 
 // reads a file dropped onto the top of the minesweeper board
-async function dropHandler(ev) {
-    console.log('File(s) dropped');
 
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-
-    if (ev.dataTransfer.items) {
-        console.log("Using Items Data Transfer interface");
-        // Use DataTransferItemList interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-            // If dropped items aren't files, reject them
-            if (ev.dataTransfer.items[i].kind === 'file') {
-                var file = ev.dataTransfer.items[i].getAsFile();
-                console.log('... file[' + i + '].name = ' + file.name);
-
-                if (file.name.endsWith(".mbf") || file.name.endsWith(".abf")) {
-                    if (!analysisMode) {
-                        newGameFromBlob(file);
-                        break; // only process the first one
-                    }
-                } else {
-                    if (analysisMode) {
-                        newBoardFromFile(file);
-                        break; // only process the first one
-                    }
-                }
-  
-            }
-        }
-    } else {
-        // Use DataTransfer interface to access the file(s)
-        console.log("File Transfer Interface not supported");
-        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-        }
-    }
-}
 
 // Prevent default behavior (Prevent file from being opened)
 function dragOverHandler(ev) {
